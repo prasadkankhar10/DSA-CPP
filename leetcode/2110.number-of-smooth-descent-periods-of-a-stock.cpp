82c33668cb0c6e51b8daab4099d9c15b2c5ff6ef
Prasad Kankhar <159956495+prasadkankhar10@users.noreply.github.com>
2025-12-15 12:25:29 +0530

    leetcode: Add solution for problem 2110

---
+++ leetcode/2110.number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=2110 lang=cpp
+ *
+ * [2110] Number of Smooth Descent Periods of a Stock
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long result = 1;
+        long long count = 1;
+
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i - 1] - prices[i] == 1)
+                count++;
+            else
+                count = 1;
+            
+            result += count;
+        }
+        return result;
+    }
+};
+// @lc code=end
+
